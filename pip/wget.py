8b27225b42b37979d0fce7819ef7d62cfe0529a8
Rogerio Hilbert <rogehil@gmail.com>
2021-10-02 19:55:24 +0100

    Fix wget

--- pip/wget.py
+++ pip/wget.py
@@ -1,17 +1,32 @@
+import re
+
 from pip.shell import ShellClient
 
 
 class Wget(object):
+
+    status_regex = re.compile(r'^\s*\w+/[\w\.]+ (\d+) \w+\s*$')
 
     def __init__(self, shell=None):
         self.shell = shell or ShellClient()
 
     def download(self, url, location):
         output = self.shell.run('wget -Sq %s -P %s' % (url, location))
-        lines = [l for l in output.splitlines() if l and l.startswith('  ')]
-        http_status = int(lines.pop(0).split()[1])
+        all_lines = [l for l in output.splitlines() if l and l.startswith('  ')]
+        header_blocks = []
+        header_lines = []
+        for line in all_lines:
+            match = self.status_regex.match(line)
+            if match:
+                http_status = int(match.group(1))
+                header_lines = []
+                header_blocks.append((http_status, header_lines))
+                continue
+            header_lines.append(line)
+
+        last_http_status, last_header_lines = header_blocks[-1]
         header = dict([[i.strip() for i in l.split(':', 1)]
-                       for l in lines if l and l.startswith('  ')])
-        if http_status != 200:
+                       for l in last_header_lines if l and l.startswith('  ')])
+        if last_http_status != 200:
             raise Exception('Failed to to download %s. %s' % (url, output))
         return header, output
